d2cd660b823a9180a6412748e9bddf39212a2a99
pli <unknown>
2019-05-08 09:50:23 +0000

    [backport] 8223427: [TESTBUG] Disable JTReg Shenandoah tests when Graal is enabled Reviewed-by: shade

--- test/hotspot/jtreg/gc/shenandoah/TestHumongousThreshold.java
+++ test/hotspot/jtreg/gc/shenandoah/TestHumongousThreshold.java
@@ -24,7 +24,7 @@
 /*
  * @test TestHumongousThreshold
  * @key gc
- * @requires vm.gc.Shenandoah
+ * @requires vm.gc.Shenandoah & !vm.graal.enabled
  *
  * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xmx1g                                      -XX:+ShenandoahVerify TestHumongousThreshold
  * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xmx1g -XX:ShenandoahHumongousThreshold=50  -XX:+ShenandoahVerify TestHumongousThreshold
