9984bb7b4b83a1f80450821c0f795fa3c13d9e7d
rkennke <unknown>
2019-09-25 12:33:05 +0200

    [backport] 8231447: Shenandoah: Compilation-time regression after JDK-8231086 Reviewed-by: shade

--- src/hotspot/share/gc/shenandoah/shenandoahBarrierSet.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahBarrierSet.cpp
@@ -24,6 +24,7 @@
 #include "precompiled.hpp"
 #include "gc/shenandoah/shenandoahAsserts.hpp"
 #include "gc/shenandoah/shenandoahBarrierSet.hpp"
+#include "gc/shenandoah/shenandoahBarrierSetClone.inline.hpp"
 #include "gc/shenandoah/shenandoahBarrierSetAssembler.hpp"
 #include "gc/shenandoah/shenandoahCollectorPolicy.hpp"
 #include "gc/shenandoah/shenandoahHeap.inline.hpp"
@@ -244,3 +245,8 @@ void ShenandoahBarrierSet::on_thread_detach(JavaThread* thread) {
     gclab->retire();
   }
 }
+
+void ShenandoahBarrierSet::clone_barrier_runtime(oop src) {
+  clone_barrier(src);
+}
+
