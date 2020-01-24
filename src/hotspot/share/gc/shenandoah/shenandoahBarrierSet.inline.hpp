36b8f5d1dec88e061170c4de39c49ec146cac65d
shade <unknown>
2020-01-24 21:11:31 +0100

    [backport] 8230853: Shenandoah: replace leftover assert(is_in(...)) with rich asserts [backport] 8237821: Shenandoah: build broken after JDK-8237637 (Remove dubious type conversions from oop) Reviewed-by: shade Contributed-by: Aditya Mandaleeka <adityam@microsoft.com>

--- src/hotspot/share/gc/shenandoah/shenandoahBarrierSet.inline.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahBarrierSet.inline.hpp
@@ -274,7 +274,7 @@ void ShenandoahBarrierSet::arraycopy_work(T* src, size_t count) {
     T o = RawAccess<>::oop_load(elem_ptr);
     if (!CompressedOops::is_null(o)) {
       oop obj = CompressedOops::decode_not_null(o);
-      if (HAS_FWD && cset->is_in((HeapWord *) obj)) {
+      if (HAS_FWD && cset->is_in(obj)) {
         assert(_heap->has_forwarded_objects(), "only get here with forwarded objects");
         oop fwd = resolve_forwarded_not_null(obj);
         if (EVAC && obj == fwd) {
