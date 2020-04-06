33472676c1ca7b0180f8e2dc301727abcdbaa43f
shade <unknown>
2020-04-06 10:31:07 +0200

    [backport] 8242114: Shenandoah: remove ShenandoahHeapRegion::reset_alloc_metadata_to_shared Reviewed-by: rkennke

--- src/hotspot/share/gc/shenandoah/shenandoahMarkCompact.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahMarkCompact.cpp
@@ -862,7 +862,7 @@ public:
     }
 
     r->set_live_data(live);
-    r->reset_alloc_metadata_to_shared();
+    r->reset_alloc_metadata();
     _live += live;
   }
 
@@ -928,7 +928,7 @@ void ShenandoahMarkCompact::compact_humongous_objects() {
             r->set_top(r->end());
           }
 
-          r->reset_alloc_metadata_to_shared();
+          r->reset_alloc_metadata();
         }
       }
     }
