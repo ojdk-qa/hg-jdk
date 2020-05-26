4eb9ea40b0dbdc144117d613d58b68c9afe5eaba
shade <unknown>
2020-05-26 09:30:17 +0200

    [backport] 8245754: Shenandoah: ditch ShenandoahAlwaysPreTouch Reviewed-by: rkennke

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -143,7 +143,7 @@ jint ShenandoahHeap::initialize() {
 
   size_t reg_size_bytes = ShenandoahHeapRegion::region_size_bytes();
 
-  if (ShenandoahAlwaysPreTouch) {
+  if (AlwaysPreTouch) {
     // Enabled pre-touch means the entire heap is committed right away.
     init_byte_size = max_byte_size;
   }
@@ -306,9 +306,7 @@ jint ShenandoahHeap::initialize() {
     _free_set->rebuild();
   }
 
-  if (ShenandoahAlwaysPreTouch) {
-    assert(!AlwaysPreTouch, "Should have been overridden");
-
+  if (AlwaysPreTouch) {
     // For NUMA, it is important to pre-touch the storage under bitmaps with worker threads,
     // before initialize() below zeroes it with initializing thread. For any given region,
     // we touch the region and the corresponding bitmaps from the same thread.
